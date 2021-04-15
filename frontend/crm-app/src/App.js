178316a3f4f210c935c3802507abaf3b413b2689
Jurgita-Sta <51950303+Jurgita-Sta@users.noreply.github.com>
2021-04-15 14:12:56 +0300

    Tuscias crm-app projektas su struktura

---
+++ frontend/crm-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  document.title = 'crm-app';
+  
+  return (
+    <div>
+      <h1>
+        Hello World
+      </h1> 
+    </div>
+  );
+}
