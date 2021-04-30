1d53f3047336f7138cc537ac8e287fe0ce0a2003
Jurgita-Sta <51950303+Jurgita-Sta@users.noreply.github.com>
2021-04-30 18:11:49 +0300

    Uzkomentuota registracija

--- frontend/crm-app/src/pages/SignIn.js
+++ frontend/crm-app/src/pages/SignIn.js
@@ -4,17 +4,17 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
+// import Link from '@material-ui/core/Link';
+// import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Modal from '@material-ui/core/Modal'
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
-import Login from '../components/Login';
+// import Modal from '@material-ui/core/Modal'
+// import Backdrop from '@material-ui/core/Backdrop';
+// import Fade from '@material-ui/core/Fade';
+// import Login from '../components/Login';
 import axios from 'axios';
 
 function Copyright() {
@@ -79,15 +79,15 @@ export default function SignIn() {
      .catch((err) => {console.log(`error!!!!!!!!`,err)})
   }
 
-  const [open, setOpen] = React.useState(false);
+  // const [open, setOpen] = React.useState(false);
 
-    const handleOpen = () => {
-    setOpen(true);
-  };
+  //   const handleOpen = () => {
+  //   setOpen(true);
+  // };
 
-    const handleClose = () => {
-    setOpen(false);
-  };
+  //   const handleClose = () => {
+  //   setOpen(false);
+  // };
 
   return (
     <div>
@@ -134,7 +134,7 @@ export default function SignIn() {
           >
             Prisijungti
           </Button>
-          <Grid container>
+          {/* <Grid container>
             <Grid item>
               <Link onClick={handleOpen} variant="body2">
                 {"Neturite paskytos? Užsiregistruokite"}
@@ -156,7 +156,7 @@ export default function SignIn() {
               </Fade>
                 </Modal>
             </Grid>
-          </Grid>
+          </Grid> */}
         </form>
       </div>
       <Box mt={8}>
