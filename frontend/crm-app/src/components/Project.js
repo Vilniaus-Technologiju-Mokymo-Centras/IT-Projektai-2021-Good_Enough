02e64c99a33e64b9c8c1b163f39c7bf2de6478a3
Jurgita-Sta <51950303+Jurgita-Sta@users.noreply.github.com>
2021-05-23 13:49:54 +0300

    Update Project.js

--- frontend/crm-app/src/components/Project.js
+++ frontend/crm-app/src/components/Project.js
@@ -1,43 +1,41 @@
-import * as React from 'react';
+import React, {useState, useEffect} from "react";
 import { DataGrid } from '@material-ui/data-grid';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
 export default function Project() {
-  return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
-    </div>
-  );
+//const [isLoaded,setIsLoaded] = useState(false);
+const [rowData,setRowData] = useState([]);
+useEffect(() => {
+const axios = require('axios').default;
+axios
+.get('http://localhost:8080/api/projects')
+.then((response) => {
+//setIsLoaded(true);
+console.log(response.data);
+setRowData(response.data);
+response.data.forEach(user => {
+});
+});
+}, []);
+const columns = [
+{ field: "id", headerName: "ID", width: 10 },
+{ field: "projectName", headerName: "Pavadinimas ", width: 150 },
+{ field: "projectDescription", headerName: "Aprašymas", width: 250 },
+{ field: "projectStatus", headerName: "Statusas", width: 150 },
+{ field: "userRole", headerName: "Užduotys", width: 150 },
+];
+return(
+<div style={{ height: 400, width: '100%' }}>
+<div style={{ display: 'flex', height: '100%' }}>
+<div style={{ flexGrow: 1 }}>
+<DataGrid
+  columns={columns}
+  rows={rowData}
+  id="id"
+  pageSize={15}
+  
+/>
+</div>
+</div>
+</div>
+);
 }
